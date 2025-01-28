6c58a14d1a17bc9fb11acada6a83f8b084b63c04
Arseniy Rauba <155465667+ArseniyRauba@users.noreply.github.com>
2025-01-28 17:12:36 +0300

    Add files via upload

---
+++ HelloWorld1/Lab_02/Lab_02.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	char hello[] = "Hello, ";	
+	char fio[] = "Rauba Arseniy Vladimirovich";
+	cout << hello << fio << endl;
+	return 0;
+}
+	
